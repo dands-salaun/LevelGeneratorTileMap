5b019022360bb43c38f47ddb6799db59dcebc6d7
Dandara Salaun <43423357+dands-salaun@users.noreply.github.com>
2020-03-25 19:28:50 -0300

    Fix order in layer
    - Add character

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 _movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _movement * (speed * Time.fixedDeltaTime));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# LevelGeneratorTileMap
- Using a `Texture2D` as a way to represent tilemapped 2D levels. 
+### Level Generator TileMap
+Using a `Texture2D` as a way to represent tilemapped 2D levels.
+
+An easy way to generate levels using tilemap settings.
+Just add the component `TileMapBuilder` to the tile map.
+Works on multiple layers, just use one image for each layer.
